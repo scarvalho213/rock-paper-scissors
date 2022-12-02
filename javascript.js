2d781d6e617ca8ce5868cd8b3b1248f7033f0280
Samuel <carvalho.isamuel@gmail.com>
2022-12-02 15:39:10 -0500

    Completed rest of assignment. Very bare bones but completed the task. playround function also adds a ++ counter to player or computer win count which is a const outside of the function starting at 0. created game function which calls play round function and loops through 5 rounds while letting the player make a different choice each round and computer randomly generating a new choice as well. At the end of each round it will display results. After the for loop of 5 rounds, if/else if statements will check whether the player or computer had more wins and will console.log the results

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+let playerWinCount = 0;
+let computerWinCount = 0;
+
 function getComputerChoice() {
     let result = Math.floor(Math.random() * 3);
     if (result == 0) {
@@ -18,12 +21,6 @@ function capitalize(string) {
     return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 }
 
-const computerSelection = getComputerChoice();
-const playerSelection = capitalize(prompt("Choose rock, paper or scissors:"));
-
-console.log(playRound(playerSelection, computerSelection));
-
-
 function playRound(playerSelection, computerSelection) {
     let playerWin = "You win!";
     let computerWin = "You lost!";
@@ -32,21 +29,47 @@ function playRound(playerSelection, computerSelection) {
         return tied;
     }
     else if (playerSelection == "Rock" && computerSelection == "Paper") {
+        computerWinCount++;
         return computerWin;
     }
     else if (playerSelection == "Rock" && computerSelection == "Scissors") {
+        playerWinCount++;
         return playerWin;
     }
     else if (playerSelection == "Paper" && computerSelection == "Scissors") {
+        computerWinCount++;
         return computerWin;
     }
     else if (playerSelection == "Paper" && computerSelection == "Rock") {
+        playerWinCount++;
         return playerWin;
     }
     else if (playerSelection == "Scissors" && computerSelection == "Rock") {
+        computerWinCount++;
         return computerWin;
     }
     else if (playerSelection == "Scissors" && computerSelection == "Paper") {
+        playerWinCount++;
         return playerWin;
     }
 }
+
+function game() {
+    for(let i = 0; i < 5; i++) {
+        const playerSelection = capitalize(prompt("Choose rock, paper or scissors:"));
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+    }
+    if (playerWinCount > computerWinCount) {
+        console.log("Congratulations, you won the game!");
+    }
+    else if (computerWinCount > playerWinCount) {
+        console.log("Sorry, you lost the game!");
+    }
+    else {
+        console.log("It was a tie");
+    }
+    
+} 
+
+console.log(game());
